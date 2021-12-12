b3632edac7df85bd32dff6dca5379c58a730f298
Zach <zawood11@gmail.com>
2021-12-11 21:16:38 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,13 @@ function Home() {
   );
 }
 
+function About() {
+  return ( 
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 {/* write an <About> component here */}
 
 
@@ -23,8 +30,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
